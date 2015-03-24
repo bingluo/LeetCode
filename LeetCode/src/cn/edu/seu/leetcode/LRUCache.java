17f500d6149d69f2eb50a8aaa1dbf54f0b2cfd03
BingLuo <BingLuo@BingLuo-PC>
2015-03-24 21:41:59 +0800

    oooops

---
+++ LeetCode/src/cn/edu/seu/leetcode/LRUCache.java
@@ -0,0 +1,32 @@
+package cn.edu.seu.leetcode;
+
+import java.util.LinkedHashMap;
+
+//trick method: using LinkedHashMap
+public class LRUCache {
+	private LinkedHashMap<Integer, Integer> cacheMap;
+	private final int capacity;
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+		cacheMap = new LinkedHashMap<>();
+	}
+
+	public int get(int key) {
+		Integer value = cacheMap.remove(key);
+		if (value == null) {
+			return -1;
+		}
+		cacheMap.put(key, value);
+		return value;
+	}
+
+	public void set(int key, int value) {
+		if (cacheMap.containsKey(key)) {
+			cacheMap.remove(key);
+		} else if (cacheMap.size() == capacity) {
+			cacheMap.remove(cacheMap.keySet().iterator().next());
+		}
+		cacheMap.put(key, value);
+	}
+}
