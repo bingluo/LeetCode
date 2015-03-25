bf69296ebbf7d7001a36c017ac768d24fc471222
bingluo <bingluo@foxmail.com>
2015-03-25 17:41:44 +0800

    Add Two Numbers

---
+++ LeetCode/src/cn/edu/seu/leetcode/AddTwoNumbers.java
@@ -0,0 +1,45 @@
+package cn.edu.seu.leetcode;
+
+public class AddTwoNumbers {
+	// Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	private int carry = 0;
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		if (l1 == null || l2 == null) {
+			if (l1 == null && l2 == null) {
+				if (carry == 0)
+					return null;
+				else
+					return new ListNode(carry);
+			} else if (l1 == null) {
+				int val = carry + l2.val;
+				carry = val / 10;
+				ListNode node = new ListNode(val % 10);
+				node.next = addTwoNumbers(null, l2.next);
+				return node;
+			} else {
+				int val = carry + l1.val;
+				carry = val / 10;
+				ListNode node = new ListNode(val % 10);
+				node.next = addTwoNumbers(l1.next, null);
+				return node;
+			}
+		} else {
+			int val = l1.val + l2.val + carry;
+			carry = val / 10;
+			ListNode node = new ListNode(val % 10);
+			node.next = addTwoNumbers(l1.next, l2.next);
+			return node;
+		}
+	}
+}
