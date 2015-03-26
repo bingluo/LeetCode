597283e178c398e29d14fd49f3ff73235367cc4d
bingluo <bingluo@foxmail.com>
2015-03-26 22:13:52 +0800

    Search Insert Position

---
+++ LeetCode/src/cn/edu/seu/leetcode/SearchInsertPosition.java
@@ -0,0 +1,11 @@
+package cn.edu.seu.leetcode;
+
+public class SearchInsertPosition {
+	public int searchInsert(int[] A, int target) {
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] >= target)
+				return i;
+		}
+		return A.length;
+	}
+}
