ec4d4e9cba408405c8cdb3782a653898a678f06b
bingluo <bingluo@foxmail.com>
2015-03-27 15:26:01 +0800

    Find Peak Element

---
+++ LeetCode/src/cn/edu/seu/leetcode/FindPeakElement.java
@@ -0,0 +1,14 @@
+package cn.edu.seu.leetcode;
+
+public class FindPeakElement {
+	public int findPeakElement(int[] num) {
+		if (num.length == 1)
+			return 0;
+
+		for (int i = 0; i < num.length; i++) {
+			if (i == num.length - 1 || num[i] > num[i + 1])
+				return i;
+		}
+		return -1;
+	}
+}
