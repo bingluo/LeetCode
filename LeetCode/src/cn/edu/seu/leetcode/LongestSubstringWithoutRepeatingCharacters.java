e52d9736d2fa627574da28124bfe1facccc8ecfa
bingluo <bingluo@foxmail.com>
2015-04-09 00:14:54 +0800

    version 2.0 concise

--- LeetCode/src/cn/edu/seu/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ LeetCode/src/cn/edu/seu/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,8 +2,8 @@ package cn.edu.seu.leetcode;
 
 import java.util.HashMap;
 
-//
 public class LongestSubstringWithoutRepeatingCharacters {
+	// version 1.0
 	public int lengthOfLongestSubstring(String s) {
 		if (s.length() == 0)
 			return 0;
@@ -31,5 +31,27 @@ public class LongestSubstringWithoutRepeatingCharacters {
 			}
 		}
 		return resultEIndex - resultSIndex + 1;
+	}
+
+	// version 2.0
+	public int lengthOfLongestSubstringV2(String s) {
+		if (s.length() == 0)
+			return 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		int maxLen = 1;
+		int startIndex = 0;
+		int endIndex = 0;
+		map.put(s.charAt(0), 0);
+		for (int i = 1; i < s.length(); i++) {
+			Integer preIndex = map.get(s.charAt(i));
+			map.put(s.charAt(i), i);
+			if (preIndex != null && preIndex >= startIndex) {
+				startIndex = preIndex + 1;
+			}
+			endIndex = i;
+			maxLen = maxLen < endIndex - startIndex + 1 ? endIndex - startIndex
+					+ 1 : maxLen;
+		}
+		return maxLen;
 	}
 }
