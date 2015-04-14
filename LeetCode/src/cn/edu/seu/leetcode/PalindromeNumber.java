e6974a1b370249868c9a7a866361d759553ebd1e
bingluo <bingluo@foxmail.com>
2015-04-14 22:40:14 +0800

    Palindrome Number

---
+++ LeetCode/src/cn/edu/seu/leetcode/PalindromeNumber.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.leetcode;
+
+public class PalindromeNumber {
+	public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		int y = 0;
+		int temp = x;
+		while (temp != 0) {
+			y *= 10;
+			y += temp % 10;
+			temp /= 10;
+		}
+		return x == y;
+	}
+}
